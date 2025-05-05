100ad22f57d14f1cbb6c7061b2f0b4573fb9466c
kabweh <135067077+kabweh@users.noreply.github.com>
2025-05-05 09:25:51 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,2 @@
-"""
-__init__.py file for upload_handlers package.
-Makes the package importable and exposes key classes.
-"""
-from upload_manager import UploadManager
-from image_handler import ImageHandler
-from pdf_handler import PDFHandler
-from docx_handler import DOCXHandler
-
-__all__ = ['UploadManager', 'ImageHandler', 'PDFHandler', 'DOCXHandler']
+# ai_tutor_project/__init__.py
+# Package initializer
